bd119bffe3ccebe8032797638d5588744f4d1994
starvoid_dev <50586934+voidstardev@users.noreply.github.com>
2023-02-26 20:19:36 +0100

    Create gggg.lua

---
+++ kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/hwid/gggg.lua
@@ -0,0 +1,15 @@
+local player = game.Players.LocalPlayer
+
+if player.Name == "RobloxFan20076" and "dontlol90901" and "bobthedummy404" then
+        game:GetService("StarterGui"):SetCore(
+            "SendNotification",
+            {
+                Title = "Whitelist Rendszer",
+                Duration = 8,
+                Text = "Sikeresen átléptél a\n\nwhitelist rendszer teszten!"
+            }
+        )
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/voidstardev/novaleypremium/main/kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj%20dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/premzgfhfdffg.lua"))()
+        else
+            player:Kick("Nem vagy" .. " rajta a whitelist-en!")
+        end
