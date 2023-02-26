c8521164e34d9948b1fa14ff1fbbff285d69eb23
starvoid_dev <50586934+voidstardev@users.noreply.github.com>
2023-02-26 20:42:41 +0100

    updatelol

--- kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/hwid/gggg.lua
+++ kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/hwid/gggg.lua
@@ -1,15 +1,54 @@
-local player = game.Players.LocalPlayer
+local player = {
+    "RobloxFan20076",
+    "dontlol90901",
+    "bobthedummy404",
+    "TheBoweeR",
+    "dontlol404",
+}
 
-if player.Name == "RobloxFan20076" and "dontlol90901" and "TheSecreLX" and "bobthedummy404" and "TheBoweeR" then
+if game.Players.LocalPlayer.Name == unpack(player) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/voidstardev/novaleypremium/main/kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj%20dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/premzgfhfdffg.lua"))()
+    else
         game:GetService("StarterGui"):SetCore(
             "SendNotification",
             {
                 Title = "Whitelist Rendszer",
                 Duration = 8,
-                Text = "Sikeresen átléptél a\n\nwhitelist rendszer teszten!"
+                Text = "Sikertelen betöltés, lehetséges\n\nem vagy hozzáadva."
             }
         )
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/voidstardev/novaleypremium/main/kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj%20dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/premzgfhfdffg.lua"))()
-        else
-            player:Kick("Nem vagy" .. " rajta a whitelist-en!")
-        end
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--local player = game.Players.LocalPlayer
+--
+--if player.Name == "RobloxFan20076" and "dontlol90901" and "TheSecreLX" and "bobthedummy404" and "TheBoweeR" then
+ --       game:GetService("StarterGui"):SetCore(
+  --          "SendNotification",
+  --          {
+   --             Title = "Whitelist Rendszer",
+    --            Duration = 8,
+     --           Text = "Sikeresen átléptél a\n\nwhitelist rendszer teszten!"
+     --       }
+     --   )
+      --  loadstring(game:HttpGet("https://raw.githubusercontent.com/voidstardev/novaleypremium/main/kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj%20dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/premzgfhfdffg.lua"))()
+      --  else
+      --      player:Kick("Nem vagy" .. " rajta a whitelist-en!")
+       -- end
