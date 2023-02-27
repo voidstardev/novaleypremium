c28d131e735a27596ab54be5810a6562d26159e4
starvoid_dev <50586934+voidstardev@users.noreply.github.com>
2023-02-27 16:46:25 +0100

    Create gggg.lua

---
+++ kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/hwid/gggg.lua
@@ -0,0 +1,22 @@
+playername = game.Players.LocalPlayer.Name
+playerid = game.Players.LocalPlayer.UserId
+
+local NotifLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))()
+local WhitelistNotify = NotifLib.Notify
+
+local whitelist = {'RobloxFan20076', 'dontlol404', 'dontlol90901', 'NightWolfs51', 'TheBoweeR'}
+if table.find(whitelist, playername) then
+        WhitelistNotify({
+            Title = "Whitelist Rendszer",
+            Description = "Sikeresen átmentél a whitelist teszten!\ndiscord.io/convetaexploit",
+            Duration = 3
+        })
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/voidstardev/novaleypremium/main/kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj%20dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/premzgfhfdffg.lua"))()
+else
+        WhitelistNotify({
+            Title = "Whitelist Rendszer",
+            Description = "Nem vagy hozzáadva!\nVásárold meg itt:\ndiscord.io/convetaexploit",
+            Duration = 3
+        })
+        setclipboard(game:GetService("RbxAnalyticsService"):GetClientId() .. "| Ezt kell sendelni amikor meg buyoltad a wl-t!")
+end
