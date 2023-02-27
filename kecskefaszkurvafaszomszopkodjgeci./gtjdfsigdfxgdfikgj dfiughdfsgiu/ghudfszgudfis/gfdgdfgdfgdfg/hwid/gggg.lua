a54fe3f2a5fff8f6b81e165b7ea5f918bd921c15
starvoid_dev <50586934+voidstardev@users.noreply.github.com>
2023-02-27 14:46:33 +0100

    jghjfghjkfghjgh

--- kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/hwid/gggg.lua
+++ kecskefaszkurvafaszomszopkodjgeci./gtjdfsigdfxgdfikgj dfiughdfsgiu/ghudfszgudfis/gfdgdfgdfgdfg/hwid/gggg.lua
@@ -23,4 +23,4 @@ game:GetService("StarterGui"):SetCore(
 	}
 )
 setclipboard(game:GetService("RbxAnalyticsService"):GetClientId() .. "| Ezt kell sendelni amikor meg buyoltad a wl-t!")
---local xenurionpwl = loadstring(game:HttpGet("https://raw.githubusercontent.com/voidstardev/xenurionloader/main/loadstring.lua"))()
+--local xenurionpwl = loadstring(game:HttpGet("https://raw.githubusercontent.com/voidstardev/xenurionloader/main/loadstring.lua"))()premium
